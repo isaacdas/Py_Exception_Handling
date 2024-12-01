9e0982bc8940ffb7db9bc982ce2006fbce9b8364
isaacdas <isaacdas10@gmail.com>
2024-12-01 14:05:23 -0500

    Add files via upload

---
+++ Exceptional_Weather_Forecast.py
@@ -0,0 +1,22 @@
+#Temperature converter
+
+
+
+
+def temp_convert(temperature):
+    new_temp = (temperature - 32) * 5/9
+    print(f"\n{temperature}°F converted to celsius is: {new_temp}°C")
+
+while True:
+    try:
+        temperature_f = int(input("\nEnter the temperature in Fahrenheit (whole number please): "))
+
+    except ValueError:
+        print("\nYou did not enter a valid input")
+    
+    else: 
+        temp_convert(temperature_f)
+    
+    finally:
+        print("Thank you for using this program. Goodbye for now.\n")
+        break
